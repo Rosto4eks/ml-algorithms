07134688d08f0adb11142443f16aa187357c5f0e
Rosto4eks <rosto4eks@mail.ru>
2024-07-04 17:00:30 +0300

    added naive bayes classifier

--- classification/naive bayes/NB.py
+++ classification/naive bayes/NB.py
@@ -1,8 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Gaussian discriminant analysis
-class GDA:
+# Gaussian Naive Bayes
+class GaussianNB:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -11,26 +11,25 @@ class GDA:
         self.shape = x.shape[1]
 
         self.means = []
-        self.cov_matrices = []
+        self.stds = []
         
         for i in range(self.num_classes):
             arr = self.x[self.y[:] == i]
-            self.means.append(arr.sum(axis=0) / len(arr))
-            self.cov_matrices.append(np.cov(arr.T))
+            self.means.append(arr.mean(axis=0))
+            self.stds.append(arr.std(axis=0))
             
     
     def predict(self, x):
         predicts = []
         for i in range(self.num_classes):
             mean = self.means[i]
-            sigma = self.cov_matrices[i]
+            std = self.stds[i]
+            pxy = 1
+            for index, feature in enumerate(x):
+                pxy *= np.exp(- np.square(feature - mean[index]) / (2 * np.square(std[index]))) / np.sqrt(2 * np.pi * np.square(std[index]))
             
-            det = np.linalg.det(sigma)
-            inv = np.linalg.inv(sigma)
-            norm_coeff = 1.0 / np.sqrt(np.power((2*np.pi), self.shape) * det)
-    
-            x_mu = np.matrix(x - mean)
-            exponent = np.exp( - x_mu * inv * x_mu.T / 2)
-            predicts.append((norm_coeff * exponent))
+            py = np.sum(self.y == i) / self.size
+
+            predicts.append((pxy * py))
 
         return np.argmax(predicts), predicts
---
+++ classification/naive bayes/test.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import NB
+
+x = np.array(
+    [[np.random.normal(1.5, 0.5), np.random.normal(1.5, 0.5)] for _ in range(50)] +
+    [[np.random.normal(3, 0.4), np.random.normal(3, 0.2)] for _ in range(50)] +
+    [[np.random.normal(5, 0.5), np.random.normal(2, 0.5)] for _ in range(60)] +
+    [[np.random.normal(3, 0.3), np.random.normal(2, 0.3)] for _ in range(70)] +
+    [[np.random.normal(4, 0.5), np.random.normal(0, 0.3)] for _ in range(40)]
+)
+
+y = np.array(
+    [0 for _ in range(50)] +
+    [1 for _ in range(50)] +
+    [2 for _ in range(60)] +
+    [3 for _ in range(70)] +
+    [4 for _ in range(40)]
+)
+
+gda = NB.GaussianNB(x, y)
+print(gda.predict([3.0, 1.0]))
+
+plt.plot(x[0:50].T[0], x[0:50].T[1], 'o', color="red")
+plt.plot(x[50:100].T[0], x[50:100].T[1], 'o', color="green")
+plt.plot(x[100:160].T[0], x[100:160].T[1], 'o', color="blue")
+plt.plot(x[160:230].T[0], x[160:230].T[1], 'o', color="yellow")
+plt.plot(x[230:270].T[0], x[230:270].T[1], 'o', color="purple")
+plt.show()
